be2c3e8d8ef69f8e0c632ca7e8705db1d2d3266f
Åsmund Wien <asmundwien@Asmunds-MacBook-Pro.local>
2018-03-28 03:57:18 +0200

    glemte en fil...

---
+++ src/model/weapons/BulletHit.java
@@ -0,0 +1,31 @@
+package model.weapons;
+
+import assets.java.Sprite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum BulletHit {
+    PLAYER_BASIC( new Sprite[] {
+            Sprite.WEAPON_PLAYER_BASIC_DMG_1,
+            Sprite.WEAPON_PLAYER_BASIC_DMG_2,
+            Sprite.WEAPON_PLAYER_BASIC_DMG_3,
+            Sprite.WEAPON_PLAYER_BASIC_DMG_4
+    }),
+    ENEMY_BASIC( new Sprite[] {
+            Sprite.WEAPON_ENEMY_BASIC_DMG_1,
+            Sprite.WEAPON_ENEMY_BASIC_DMG_2,
+            Sprite.WEAPON_ENEMY_BASIC_DMG_3,
+            Sprite.WEAPON_ENEMY_BASIC_DMG_4
+    });
+
+    public final Sprite[] HIT_SPRITES;
+
+    BulletHit(Sprite[] list){
+        HIT_SPRITES = list;
+    }
+
+    public Sprite[] getSprites(){
+        return HIT_SPRITES;
+    }
+}
