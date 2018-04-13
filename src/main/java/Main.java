daf8d003238b160166a7cbc3263745ab9b6b6be4
Jonas-C <jonas.c@hotmail.no>
2018-04-14 01:09:52 +0200

    Startet on a menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,10 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.GameModel;
 import view.GameView;
+import view.MenuView;
 
 public class Main extends Application {
 
     public static GameView gv = GameView.getInstance();
+//    public static MenuView gm = MenuView.getInstance();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -18,6 +20,7 @@ public class Main extends Application {
 
         primaryStage.setTitle("Working Title: Pippi");
         Scene scene = new Scene(gv.initGame());
+//        Scene scene = new Scene(gm.initScene());
 
         UserInputs userInputs = new UserInputs(scene);
 
