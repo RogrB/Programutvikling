74d9af896126c8dc8c396d9238c4230b73783f40
DESKTOP-0B2FR1F\jonas <jonas.c@hotmail.no>
2018-04-17 17:01:27 +0200

    Worked on the Menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ package main.java;
 import controller.GameController;
 import controller.UserInputs;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.GameModel;
@@ -12,15 +13,20 @@ import view.MenuView;
 public class Main extends Application {
 
     public static GameView gv = GameView.getInstance();
-//    public static MenuView gm = MenuView.getInstance();
+    //public static MenuView gm = MenuView.getInstance();
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         gv.setup();
 
         primaryStage.setTitle("Working Title: Pippi");
+        primaryStage.setResizable(false);
+        primaryStage.setOnCloseRequest(e -> {
+            System.out.println(java.lang.Thread.activeCount());
+            Platform.exit();
+            System.exit(0);});
         Scene scene = new Scene(gv.initGame());
-//        Scene scene = new Scene(gm.initScene());
+        //Scene scene = new Scene(gm.initScene(primaryStage));
 
         UserInputs userInputs = new UserInputs(scene);
 
--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,23 +1,32 @@
 package view;
 
+import controller.UserInputs;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import main.java.Main;
 
 public class MenuView{
 
-    private static MenuView inst = new MenuView();
-    private MenuView(){}
+    public static MenuView inst = new MenuView();
+    public MenuView(){}
     public static MenuView getInstance(){return inst; }
 
     public static final int MENU_WIDTH = 1200;
     public static final int MENU_HEIGHT = 800;
     private static final String BG_IMG = "assets/image/background.jpg";
+    public Pane root;
+    public Stage stage;
 
     public Background getBackGroundImage(){
         BackgroundImage bg = new BackgroundImage(
@@ -37,16 +46,36 @@ public class MenuView{
         return new Background(bg);
     }
 
-    public Parent initScene(){
-        Pane root = new Pane();
-        VBox menuContent = new VBox();
-        root.setPrefSize(MENU_WIDTH, MENU_HEIGHT);
-        root.setBackground(getBackGroundImage());
-        Button startButton = new Button("Start");
-        Button optionsButton = new Button("Options");
-        Button exitButton = new Button("Exit");
-        menuContent.getChildren().addAll(startButton, optionsButton, exitButton);
-        root.getChildren().addAll(menuContent);
+    public void createNewGame(KeyEvent event){
+        if(event.getCode() == KeyCode.ENTER || event.getCode() == KeyCode.SPACE){
+            GameView.getInstance().setup();
+            Scene scene = new Scene(GameView.getInstance().initGame());
+            stage.setScene(scene);
+            UserInputs userInputs = new UserInputs(scene);
+            System.out.println("Totally started a new game");
+        }
+    }
+
+    public void showLevelSelect(KeyEvent event){
+        if(event.getCode() == KeyCode.ENTER || event.getCode() == KeyCode.SPACE){
+            System.out.println("Totally showed u some tight levels");
+        }
+    }
+
+    public void showOptions(KeyEvent event){
+        if(event.getCode() == KeyCode.ENTER || event.getCode() == KeyCode.SPACE){
+            System.out.println("Totally showed u sum looney options");
+        }
+    }
+
+    public void exitGame(KeyEvent event){
+        if(event.getCode() == KeyCode.ENTER || event.getCode() == KeyCode.SPACE){
+            System.exit(0);
+        }
+    }
+
+    public Parent initScene(Stage stage) throws Exception{
+        root = FXMLLoader.load(getClass().getClassLoader().getResource("assets/fxml/MenuView.fxml"));
         return root;
     }
 }
