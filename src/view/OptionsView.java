1b7ac12c737c0912b9846179aa6fdc9cb272fdd4
DESKTOP-0B2FR1F\jonas <jonas.c@hotmail.no>
2018-04-23 18:16:06 +0200

    Options are actually usable now. They save and everything!

--- src/view/OptionsView.java
+++ src/view/OptionsView.java
@@ -1,5 +1,8 @@
 package view;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -24,15 +27,46 @@ public class OptionsView extends ViewUtil{
     private static final String BG_IMG = "assets/image/background.jpg";
     public VBox optionsMenu;
 
-    public Label volumeLabel;
-    public Label difficultyLabel;
+    public int difficultyValue = 2;
+    public int soundValue = 50;
+    public int musicValue = 50;
 
-    public Slider volumeSlider;
+    public Label soundLabel;
+    private Label soundTextLabel;
+    private Label musicTextLabel;
+    public Label musicLabel;
+    public Label difficultyLabel;
+    public Label difficultyTextLabel;
+
+    public Slider soundSlider;
+    public Slider musicSlider;
     public Slider difficultySlider;
 
     public MenuButton backButton;
 
     public OptionsView(){
+    }
+
+    public String setDifficultyText(int difficultyValue){
+        String text = "";
+        switch(difficultyValue){
+            case 0:
+                text = "EASY";
+                break;
+            case 1:
+                text = "CASUAL";
+                break;
+            case 2:
+                text = "NORMAL";
+                break;
+            case 3:
+                text = "HARD";
+                break;
+            case 4:
+                text = "INSANE";
+                break;
+        }
+        return text;
     }
 
     public Parent initScene(){
@@ -45,24 +79,50 @@ public class OptionsView extends ViewUtil{
         header.setFont(header.getFont().font(100));
         root.setPrefSize(VIEW_WIDTH, VIEW_HEIGHT);
         root.setBackground(getBackGroundImage(BG_IMG));
-        volumeLabel = new Label("VOLUME");
+        soundLabel = new Label("VOLUME");
+        soundLabel.setTextFill(Color.WHITE);
+        musicLabel = new Label("MUSIC");
+        musicLabel.setTextFill(Color.WHITE);
         difficultyLabel = new Label("DIFFICULTY");
-        volumeSlider = new Slider();
-        volumeSlider.setMin(0);
-        volumeSlider.setMax(100);
-        volumeSlider.setValue(40);
-        volumeSlider.setShowTickMarks(true);
-        volumeSlider.setBlockIncrement(10);
-        volumeSlider.setMinorTickCount(10);
-        difficultySlider = new Slider();
-        difficultySlider.setMin(0);
-        difficultySlider.setMax(100);
-        difficultySlider.setValue(40);
+        difficultyLabel.setTextFill(Color.WHITE);
+        difficultyTextLabel = new Label(setDifficultyText(difficultyValue));
+        difficultyTextLabel.setTranslateX(125);
+        difficultyTextLabel.setTextFill(Color.WHITE);
+        soundSlider = new Slider(0, 100, soundValue);
+        soundSlider.setShowTickMarks(true);
+        soundSlider.setMajorTickUnit(25);
+        soundSlider.setBlockIncrement(25);
+        soundTextLabel = new Label(String.valueOf(soundValue));
+        soundSlider.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            soundTextLabel.setText(String.valueOf(newValue.intValue()));
+            soundValue = newValue.intValue();
+        }));
+        soundTextLabel.setTranslateX(145);
+        soundTextLabel.setTextFill(Color.WHITE);
+        musicSlider = new Slider(0, 100, musicValue);
+        musicSlider.setShowTickMarks(true);
+        musicSlider.setMajorTickUnit(25);
+        musicSlider.setBlockIncrement(25);
+        musicTextLabel = new Label(String.valueOf(musicValue));
+        musicSlider.valueProperty().addListener(((observable, oldValue, newValue) -> {
+            musicTextLabel.setText(String.valueOf(newValue.intValue()));
+            musicValue = newValue.intValue();
+        }));
+        musicTextLabel.setTranslateX(145);
+        musicTextLabel.setTextFill(Color.WHITE);
+        difficultySlider = new Slider(0, 4, difficultyValue);
+        difficultySlider.setSnapToTicks(true);
         difficultySlider.setShowTickMarks(true);
-        difficultySlider.setBlockIncrement(10);
-        difficultySlider.setMinorTickCount(10);
+        difficultySlider.setMajorTickUnit(1);
+        difficultySlider.setMinorTickCount(0);
+        difficultySlider.setBlockIncrement(1);
+        difficultySlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            difficultySlider.setValue(newValue.intValue());
+            difficultyValue = newValue.intValue();
+            difficultyTextLabel.setText(setDifficultyText(difficultyValue));
+        });
         backButton = new MenuButton("BACK");
-        optionsMenu.getChildren().addAll(volumeLabel, volumeSlider, difficultyLabel, difficultySlider, backButton);
+        optionsMenu.getChildren().addAll(soundLabel, soundSlider, soundTextLabel, musicLabel, musicSlider, musicTextLabel, difficultyLabel, difficultySlider, difficultyTextLabel, backButton);
         optionsMenu.setSpacing(10);
         optionsMenu.setTranslateX(450);
         optionsMenu.setTranslateY(250);
