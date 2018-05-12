000e1eddb4ac0454bd33dffce2115734c6e219cd
RogerStasjonær <roger.birkenes@gmail.com>
2018-05-12 22:33:03 +0200

    Javadocs for weaponupgrades

--- src/model/weapons/DoubleSwirl.java
+++ src/model/weapons/DoubleSwirl.java
@@ -4,12 +4,43 @@ package model.weapons;
 
 import java.util.Iterator;
 
+/**
+ * <h1>A Class that handles the creation of the DoubleSwirl upgrade</h1>
+ * The {@code DoubleSwirl} class extends the {@code Basic} class to create a
+ * weapon upgrade. 
+ * This weapon upgrade fires two bullets that spins around each other
+ * in the Y axis
+ * @author Roger Birkenes Solli
+ */
 public class DoubleSwirl extends Basic {
-    
+
+    /**
+     * If the {@code this} is fired at the top or bottom. 
+     * Decides the direction of the spin
+     */    
     private boolean top;
+    
+    /**
+     * Current travel direction - up or down
+     */     
     private boolean goingUp;
+    
+    /**
+     * Counter for the amount of times {@code this} travels in the
+     * given direction. Based on ticks from the animationtimer in
+     * {@code GameController}
+     * After 6 ticks it changes direction, thus creating the "swirl" movement
+     */       
     private int swirlCounter = 0;
     
+    /**
+     * <b>Constructor: </b>sets the X and Y values
+     * for where the bullet is fired from and the {@code Weapon}
+     * @param x X Value
+     * @param y Y Value
+     * @param weapon sets the Weapon type in the {@code Weapon} class @see {@code Weapon}
+     * @param top sets if the bullet is fired at the top or bottom
+     */       
     public DoubleSwirl(int x, int y, Weapon weapon, boolean top) {
         super(
         x,
@@ -21,6 +52,12 @@ public class DoubleSwirl extends Basic {
         }
     }     
     
+    /**
+     * Overrides the {@code update} from the {@code Basic} bullets.
+     * This method moves the bullets along the X axis, as well as handles
+     * the Y movements to create the "swirl" effect.
+     * After 6 ticks from the anmationtimer it changes direction
+     */       
     @Override
     public void update(int x, int y, Iterator i) {
         this.setX(getX() + 20);
--- src/model/weapons/TripleBurst.java
+++ src/model/weapons/TripleBurst.java
@@ -4,10 +4,29 @@ package model.weapons;
 
 import java.util.Iterator;
 
+/**
+ * <h1>A Class that handles the creation of the TripleBurst upgrade</h1>
+ * The {@code TripleBurst} class extends the {@code Basic} class to create a
+ * weapon upgrade that fires thee bullets that spread out as they move across the field.
+ * @author Roger Birkenes Solli
+ */
 public class TripleBurst extends Basic {
     
+    /**
+     * Determines which position the bullet
+     * was fired from. 1 out of 3.
+     * Decides which direction it travels
+     */        
     private int level;
     
+    /**
+     * <b>Constructor: </b>sets the X and Y values
+     * for where the bullet is fired from and the {@code Weapon}
+     * @param x X Value
+     * @param y Y Value
+     * @param weapon sets the Weapon type in the {@code Weapon} class @see {@code Weapon}
+     * @param level sets the position the bullet was fired from
+     */        
     public TripleBurst(int x, int y, Weapon weapon, int level) {
         super(
         x,
@@ -16,6 +35,16 @@ public class TripleBurst extends Basic {
         this.level = level;
     }     
     
+    /**
+     * Overrides the {@code update} from the {@code Basic} bullets
+     * to handle the update method.
+     * Moves the bullet along the X axis and changes its Y value
+     * based on where it was fired from to spread the bullets out
+     * as they travel.
+     * @param x X position 
+     * @param y Y position
+     * @param i Iterator needed to override method
+     */     
     @Override
     public void update(int x, int y, Iterator i) {
         this.setX(getX() + 20);
