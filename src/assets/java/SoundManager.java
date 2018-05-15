6a6a2b6382ec4fe03016046ba53a5ef378abfa59
DESKTOP-0B2FR1F\jonas <jonas.c@hotmail.no>
2018-05-16 00:22:50 +0200

    Upped the Thread count in SoundManager

--- src/assets/java/SoundManager.java
+++ src/assets/java/SoundManager.java
@@ -40,7 +40,7 @@ public class SoundManager {
     /**
      * Executor that handles playing sounds.
      */
-    private ExecutorService soundPool = Executors.newFixedThreadPool(4);
+    private ExecutorService soundPool = Executors.newFixedThreadPool(8);
 
     /**
      * Map to store music files
